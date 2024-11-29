32779737154bf97fd39dd50bb0385fd768786319
Yoander Justesen <yoanderfl@gmail.com>
2024-11-29 18:30:06 +0100

    Projeto Guanabara

---
+++ aprendendo javascript/aula13.js
@@ -0,0 +1,19 @@
+// while teste logico no inicio:
+let c = 1
+
+while (c <= 6 ) {
+    
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+
+// while teste logico no final 
+console.log('*****************')
+
+let i = 1 
+
+do {
+    
+    console.log(`passo com Do ${i}`)
+    i++
+} while (i <= 6)
---
+++ aprendendo javascript/aula14.js
@@ -0,0 +1,4 @@
+//Repetições com FOR
+for (c = 1; c <= 10; c++) {
+    console.log(`paso ${c}`)
+}
