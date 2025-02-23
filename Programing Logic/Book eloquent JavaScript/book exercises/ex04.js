bac3f89d61522fbbf637b264ac6980300647b2e1
YOANDER FARLLEY LAEBER JUSTESEN <90282664+yozintt@users.noreply.github.com>
2025-02-23 20:40:40 +0100

    update from the book

---
+++ Programing Logic/Book eloquent JavaScript/book exercises/ex04.js
@@ -0,0 +1,15 @@
+// minimun:
+
+function min(num1, num2) {
+  return num1 <= num2 ? num1 : num2;
+
+  //   or
+  /*
+  if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+  */
+}
+console.log(min(5, 2));
---
+++ Programing Logic/Book eloquent JavaScript/book exercises/ex06.js
@@ -0,0 +1,11 @@
+// Bean counting
+function countBs(string, ch) {
+  let saveString = 0;
+  for (let c = 0; c < string.length; c++) {
+    if (string[c] == ch) {
+      saveString += 1;
+    }
+  }
+  return saveString;
+}
+console.log(countBs("BOB", "B"));
