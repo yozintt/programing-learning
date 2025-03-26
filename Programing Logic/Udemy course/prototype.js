9cf02ca1b01bd5473f55bc9031a141b95a089e00
YOANDER FARLLEY LAEBER JUSTESEN <90282664+yozintt@users.noreply.github.com>
2025-03-26 19:45:24 +0100

    update

---
+++ Programing Logic/Udemy course/prototype.js
@@ -0,0 +1,29 @@
+/*
+let pessoa = {
+  maos: 2,
+};
+let pessoaNova = Object.create(pessoa);
+console.log(pessoaNova.maos);
+console.log(Object.getPrototypeOf(pessoaNova) === pessoa);
+console.log(pessoaNova.hasOwnProperty("maos"));
+
+
+class Cachorro {
+  constructor(raca, cor) {
+    this.raca = raca;
+    this.cor = cor;
+  }
+  latir() {
+    console.log("Au Au");
+  }
+}
+
+Cachorro.prototype.patas = 4;
+
+let akita = new Cachorro("Akita", "Caramelo");
+
+console.log(akita);
+console.log(akita.patas);
+akita.latir();
+
+*/
